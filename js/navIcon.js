36df75d92b00bbddd1a39c1e652703e706b9812d
Emmanuel Perez <emmanueleperez@icloud.com>
2021-01-01 23:56:47 -0500

    first commit

---
+++ js/navIcon.js
@@ -0,0 +1,8 @@
+const navIconAction = () => {
+
+    
+    let openNav = () => {
+        console.log("Clicked");
+    }
+    document.getElementById("hamburger").addEventListener("click", openNav);
+}
