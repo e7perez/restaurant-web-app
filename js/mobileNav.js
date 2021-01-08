ac55eeaa14a22649ba54dc7879e8365abb7f72eb
Emmanuel Perez <emmanueleperez@icloud.com>
2021-01-08 17:13:00 -0500

    updated menu

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,11 +1,17 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.navigation');
-hamburger.addEventListener('click', () => {
-    if (hamburger.className === "hamburger active") {
-        hamburger.className = "hamburger";
-        mobileNav.className = "navigation";
-    } else {
-        hamburger.className = "hamburger active";
-        mobileNav.className = "navigation active";
-    }
-});
+let mobileMenu = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.navigation');
+
+    hamburger.addEventListener('click', () => {
+        if (hamburger.className === "hamburger active") {
+            hamburger.className = "hamburger";
+            mobileNav.className = "navigation";
+        } else {
+            hamburger.className = "hamburger active";
+            mobileNav.className = "navigation active";
+        }
+        
+    });
+    
+}
+mobileMenu();
